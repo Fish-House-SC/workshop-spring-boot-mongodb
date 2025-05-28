1d88ae6ec67d7783914ed93d27530a88d6898261
Fish House <estudiofishhouse@gmail.com>
2025-05-28 18:58:07 -0300

    Consulta simples com @Query

--- src/main/java/com/fish_house/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fish_house/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.fish_house.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.fish_house.workshopmongo.domain.Post;
 
 @EnableMongoRepositories
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
