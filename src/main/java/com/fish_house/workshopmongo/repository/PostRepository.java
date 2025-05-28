b7453f6d4521bb9b0e7dd9bdd785ffc4bce7d4d7
Fish House <estudiofishhouse@gmail.com>
2025-05-28 17:53:38 -0300

    Consulta simples com query methods

--- src/main/java/com/fish_house/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fish_house/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.fish_house.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -8,5 +10,5 @@ import com.fish_house.workshopmongo.domain.Post;
 @EnableMongoRepositories
 public interface PostRepository extends MongoRepository<Post, String> {
 
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
